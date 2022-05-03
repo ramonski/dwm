202225b1b9ae6cde5478306e613a9eb967746480
Ramon Bartl <rb@ridingbytes.com>
2022-05-03 20:18:00 +0200

    Apple+Q to quit program

--- config.h
+++ config.h
@@ -160,6 +160,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
+	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} },
 };
